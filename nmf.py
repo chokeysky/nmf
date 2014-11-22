f15092e16691fa0011a4981561a55ad97e8fd346
erik nyquist <eriknyquist@erik-nyquists-MacBook-Pro-2.local>
2014-11-22 23:00:34 +0000

    populate_matrix now returns document frequency (DF) matrix as well

--- nmf.py
+++ nmf.py
@@ -22,8 +22,9 @@ def malformed(msg):
 def populate_matrix():
 	'''reads in a list of terms & a term-document matrix
 	from supplied files, populates an array.
-	returns: A, terms
-	A = term-document matrix in the form of 2D array of floats.
+	returns: A, DF, terms
+	V = term-document matrix in the form of 2D array of floats.
+	DF = integer array, each entry DF[i] quantifies how many documents term i appears in
 	terms = the terms as a 1D array of strings.'''
 
 	files = ""
@@ -48,9 +49,12 @@ def populate_matrix():
 		rows, columns, numvalues = mfh.readline().split()
 
 		# initialise matrix A with above dimensions to all zeros
-		A = [[0.0 for i in range(int(rows))] for j in range(int(columns))]
+		V = [[0.0 for i in range(int(rows))] for j in range(int(columns))]
 
-		# populate matrix A
+		# initialise array to store document frequency (DF) of terms
+		DF = [0 for i in range(len(terms))]
+
+		# populate matrix V
 		count = 0
 		for line in mfh:
 			# skip empty lines- just in case!
@@ -65,24 +69,31 @@ def populate_matrix():
 			# sanity check:
 			# double entry means I did something wrong,
 			# or matrix was generated incorrectly
-			if A[doc -1][term - 1] == 0:
-				A[doc - 1][term - 1] = freq
+			if V[doc -1][term - 1] == 0:
+				V[doc - 1][term - 1] = freq
 				count += 1
 			else:
 				malformed("Entry #" + str(count + 1) + ", for term '" +
 					terms[term - 1].strip() + "' in document #" + str(doc) +
-					"\nalready has a value of " + A[doc -1][term -1] +
+					"\nalready has a value of " + V[doc -1][term -1] +
 					".\nEntries cannot be assigned twice.")
+
+			DF[term - 1] += 1
 
 		# final sanity check
 		if int(numvalues) != count:
 				malformed("Expecting " + numvalues +
 					" entries but found " + str(count))
 	print("Done.")
-	return A, terms	
+	return V, DF, terms	
+
 
 def main():
-	A, terms = populate_matrix()
-	A = numpy.array(A)
+	V, DF, terms = populate_matrix()
+	V = numpy.array(V)
+	M = len(V)
+	N = len(V[0])
+	K = 5
+	print(numpy.log10(2.0/1.0))
 
 main()
